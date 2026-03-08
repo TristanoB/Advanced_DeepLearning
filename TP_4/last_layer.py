632dad59c621f900c94486d15df0e9d3ea792c5f
Tristan BERUARD <60015898+TristanoB@users.noreply.github.com>
2026-03-08 21:09:40 +0000

    .

--- TP_4/last_layer.py
+++ TP_4/last_layer.py
@@ -1,14 +1,15 @@
 import torch.nn as nn  
 
 
-# from which torch class it must herites from ? 
-def LastLayer(nn.Layer) : 
+# the Layer must herite from nn.Module 
+class LastLayer(nn.Module) : 
     # architecture : init + forward 
     
-    def __init__(self,nn.Module,out_features=2) :
+    def __init__(self, x, nn.Module, in_features, out_features=2) :
         self.out_features = 2 
-        
-        pass
+        self.in_features = x.shape[1] # Suppose (B,D)
+        self.fc = nn.Linear()
+        self.x = x 
     
     # use a forward linear layer ? 
     def forward(self,x) :
